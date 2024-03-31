d65e4160b93831d9d8cf02c28be216bc1a57103f
davidhjensen <dhjensen02@gmail.com>
2024-03-31 14:01:51 -0600

    motor test done, integrating

--- src/motor_test.c
+++ src/motor_test.c
@@ -17,43 +17,21 @@
 #define MOTOR_EN_1 16
 #define MOTOR_EN_2 17
 
-#define SERVO_PWM_PIN 0
-#define SERVO_LIMITTER 15.0 // +- degrees that the servo is capped at; MAX 90
-#define SERVO_VOLTAGE_PIN 6 // output PWM to step ~12V down to ~6V
-
 #define MAX_JOY_Y 128.0
 #define JOY_Y_CENTER 128.0
 #define JOY_Y_BUFFER 20 // necessary deviation from center to activate motor
 
-#define MAX_JOY_X 128.0
-#define JOY_X_CENTER 128.0
-
-
-uint servo_slice;
-uint servo_voltage_slice;
 uint motor_slice;
 
 uint motor_dir_status = 0; // 0: stopped; 1: forward; 2: backward
-
-void servo_init() {
-	// servo pwm setup 
-	gpio_set_function(SERVO_PWM_PIN, GPIO_FUNC_PWM);
-	servo_slice = pwm_gpio_to_slice_num(SERVO_PWM_PIN);
-	// clk div down to 50 Hz (period is 25000 counts) (clock is 125x10^6/100)
-	pwm_set_wrap(servo_slice, 25000); 
-	pwm_set_clkdiv(servo_slice, 100);
-	// set to center at 7.5% duty
-	pwm_set_chan_level(servo_slice, 0, 1875);
-	pwm_set_enabled(servo_slice, true);
-}
 
 void motor_init() {
 	// motor pwm setup
 	gpio_set_function(MOTOR_PWM_PIN, GPIO_FUNC_PWM);
 	motor_slice = pwm_gpio_to_slice_num(MOTOR_PWM_PIN);
-	// clk div down to 1kHz (period is 31250 counts) (clock is 125x10^6/4)
-	pwm_set_wrap(motor_slice, 31250); 
-	pwm_set_clkdiv(motor_slice, 4);
+	// clk div down to 500Hz (period is 31250 counts) (clock is 125x10^6/4)
+	pwm_set_wrap(motor_slice, 31249); 
+	pwm_set_clkdiv(motor_slice, 8);
 	// set to still
 	pwm_set_chan_level(motor_slice, 0, 0);
 	pwm_set_enabled(motor_slice, true);
@@ -64,17 +42,6 @@ void motor_init() {
 	gpio_set_dir(MOTOR_EN_2, GPIO_OUT);
 	gpio_put(MOTOR_EN_1, 0);
 	gpio_put(MOTOR_EN_2, 0);
-}
-
-void servo_voltage_reg() {
-	// servo pwm setup 
-	gpio_set_function(SERVO_VOLTAGE_PIN, GPIO_FUNC_PWM);
-	servo_voltage_slice = pwm_gpio_to_slice_num(SERVO_VOLTAGE_PIN);
-	// clk div down to 10kHz
-	pwm_set_wrap(servo_voltage_slice, 12500); 
-	// set to around 5V (12V * 4000/12500)
-	pwm_set_chan_level(servo_voltage_slice, 0, 3000);
-	pwm_set_enabled(servo_voltage_slice, true);
 }
 
 uint joystickY2MotorPwm(uint8_t joy_in, uint dir_stat) {
@@ -106,47 +73,20 @@ uint joystickY2MotorPwm(uint8_t joy_in, uint dir_stat) {
 	return dir_stat;
 }
 
-void joystickX2ServoPwm(uint8_t joy_in) {
-	uint16_t val = (int) 25000 * (.075 + .05 * (SERVO_LIMITTER/90) * (joy_in-JOY_X_CENTER) / MAX_JOY_Y);
-	pwm_set_gpio_level(MOTOR_PWM_PIN, val);
-	printf("Servo PWM: (%d) %d (%.2f%%)\n", joy_in, val, 100.0*val/25000);
-}
-
 int main() {
 	stdio_init_all();
 
 	motor_init();
-	servo_voltage_reg();
-	servo_init();
 
 	sleep_ms(1000);
 	printf("Hello\n");
 
-	multicore_launch_core1(bt_main);
-	// Wait for init (should do a handshake with the fifo here?)
-	sleep_ms(1000);
-
-	struct bt_hid_state state;
-
-	for ( ;; ) {
-		sleep_ms(5000);
-		bt_hid_get_latest(&state);
-		//printf(buffer, "buttons: %04x, l: %d,%d, r: %d,%d, l2,r2: %d,%d hat: %d\n",
-		//		state.buttons, state.lx, state.ly, state.rx, state.ry,
-		//		state.l2, state.r2, state.hat);
-		motor_dir_status = joystickY2MotorPwm(state.ly, motor_dir_status);
-		joystickX2ServoPwm(state.rx);
-	}
-	/*
 	while(1) {
 		sleep_ms(1000);
 		printf("%c", 12);
-		joystickX2ServoPwm(120);
-		joystickY2MotorPwm(120);
+		motor_dir_status = joystickY2MotorPwm(200, motor_dir_status);
 		sleep_ms(1000);
 		printf("%c", 12);
-		joystickX2ServoPwm(-50);
-		joystickY2MotorPwm(-50);
+		motor_dir_status = joystickY2MotorPwm(40, motor_dir_status);
 	}
-	*/
 }
