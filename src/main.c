d2834e1754714f1f329a4746c1664992ab8eb51f
davidhjensen <dhjensen02@gmail.com>
2024-03-30 19:25:35 -0600

    print outs

--- src/main.c
+++ src/main.c
@@ -77,33 +77,33 @@ void servo_voltage_reg() {
 	pwm_set_enabled(servo_voltage_slice, true);
 }
 
-uint joystickY2MotorPwm(uint8_t joy_in, uint motor_dir_status) {
+uint joystickY2MotorPwm(uint8_t joy_in, uint dir_stat) {
 	// if it should be stopped
-	if((motor_dir_status != 0) && (joy_in <= JOY_Y_CENTER + JOY_Y_BUFFER) && (joy_in >= JOY_Y_CENTER - JOY_Y_BUFFER)) {
+	if((dir_stat != 0) && (joy_in <= JOY_Y_CENTER + JOY_Y_BUFFER) && (joy_in >= JOY_Y_CENTER - JOY_Y_BUFFER)) {
 		gpio_put(MOTOR_EN_1, 0);
 		gpio_put(MOTOR_EN_2, 0);
-		motor_dir_status = 0;
+		dir_stat = 0;
 
 	// if it should be forward
-	} else if((motor_dir_status != 1) && (joy_in > JOY_Y_CENTER + JOY_Y_BUFFER)) {
+	} else if((dir_stat != 1) && (joy_in > JOY_Y_CENTER + JOY_Y_BUFFER)) {
 		gpio_put(MOTOR_EN_1, 0);
 		gpio_put(MOTOR_EN_2, 1);
-		motor_dir_status = 1;
+		dir_stat = 1;
 		
 	// if it should be backward
-	} else if(motor_dir_status != 2) {
+	} else if(dir_stat != 2) {
 		gpio_put(MOTOR_EN_1, 1);
 		gpio_put(MOTOR_EN_2, 0);
-		motor_dir_status = 2;
+		dir_stat = 2;
 	}
 
 	// convert to PWM duty and set
 	uint16_t val = (int) 31250 * (abs(joy_in-JOY_Y_CENTER) / MAX_JOY_Y) * MOTOR_LIMITTER / 100;
 	pwm_set_gpio_level(MOTOR_PWM_PIN, val);
 	
-	printf("Motor PWM: (%d) %d (%.2f%%)\n", joy_in, val, 100.0*val/31250);
+	printf("Motor PWM: (%d) %d (%.2f%%) EN1: %d EN2: %d STATUS: %d\n", joy_in, val, 100.0*val/31250, gpio_get_out_level(MOTOR_EN_1), gpio_get_out_level(MOTOR_EN_2), dir_stat);
 
-	return motor_dir_status;
+	return dir_stat;
 }
 
 void joystickX2ServoPwm(uint8_t joy_in) {
