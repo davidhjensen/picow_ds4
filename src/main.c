abd44df081745207cf62d54cd6c687795718e8bb
davidhjensen <dhjensen02@gmail.com>
2024-03-31 19:49:50 -0600

    blah

--- src/main.c
+++ src/main.c
@@ -43,7 +43,7 @@ DHT20 sens;
 DHT20 *sens_ptr = &sens;
 
 void sensor_init() {
-	i2c_init(i2c0, 100 * 1000);
+	i2c_init(i2c_default, 100 * 1000);
     gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN, GPIO_FUNC_I2C);
     gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN, GPIO_FUNC_I2C);
     gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
